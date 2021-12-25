f9629d4f6a81defe82951223186eb2047fb77080
Cactys <git@cactys.dev>
2021-12-25 21:15:26 +0100

    Simplified exports

--- src/SampNode.ts
+++ src/SampNode.ts
@@ -1,38 +1,36 @@
-export class SampNode {
-    static on(eventName: string, func: (...args: Array<any>) => void): void {
-        samp.on(eventName, func);
-    }
-    static addListener(eventName: string, func: (...args: Array<any>) => void): void {
-        samp.addListener(eventName, func);
-    }
-    static addEventListener(eventName: string, func: (...args: Array<any>) => void): void {
-        samp.addEventListener(eventName, func);
-    }
-    static removeListener(eventName: string, func: (...args: Array<any>) => void | Array<(...args: Array<any>) => void>): void {
-        samp.removeListener(eventName, func);
-    }
-    static removeEventListener(eventName: string, func: (...args: Array<any>) => void | Array<(...args: Array<any>) => void>): void {
-        samp.removeEventListener(eventName, func);
-    }
-    static registerEvent(eventName: string, paramTypes: string): boolean {
-        return samp.registerEvent(eventName, paramTypes);
-    }
-    static fire(eventName: string, ...args: Array<any>): void {
-        samp.fire(eventName, ...args);
-    }
-    static callPublic(publicName: string, paramTypes: string, ...args: Array<any>): any {
-        return samp.callPublic(publicName, paramTypes, ...args);
-    }
-    static callPublicFloat(publicName: string, paramTypes: string, ...args: Array<any>): any {
-        return samp.callPublicFloat(publicName, paramTypes, ...args);
-    }
-    static logprint(str: string): any {
-        return samp.logprint(str);
-    }
-    static callNative(nativeName: string, paramTypes: string, ...args: Array<any>): any {
-        return samp.callNative(nativeName, paramTypes, ...args);
-    }
-    static callNativeFloat(nativeName: string, paramTypes: string, ...args: Array<any>): any {
-        return samp.callNativeFloat(nativeName, paramTypes, ...args);
-    }
+export const on = (eventName: string, func: (...args: Array<any>) => void): void => {
+    samp.on(eventName, func);
+}
+export const addListener = (eventName: string, func: (...args: Array<any>) => void): void => {
+    samp.addListener(eventName, func);
+}
+export const addEventListener = (eventName: string, func: (...args: Array<any>) => void): void => {
+    samp.addEventListener(eventName, func);
+}
+export const removeListener = (eventName: string, func: (...args: Array<any>) => void | Array<(...args: Array<any>) => void>): void => {
+    samp.removeListener(eventName, func);
+}
+export const removeEventListener = (eventName: string, func: (...args: Array<any>) => void | Array<(...args: Array<any>) => void>): void => {
+    samp.removeEventListener(eventName, func);
+}
+export const registerEvent = (eventName: string, paramTypes: string): boolean => {
+    return samp.registerEvent(eventName, paramTypes);
+}
+export const fire = (eventName: string, ...args: Array<any>): void => {
+    samp.fire(eventName, ...args);
+}
+export const callPublic = (publicName: string, paramTypes: string, ...args: Array<any>): any => {
+    return samp.callPublic(publicName, paramTypes, ...args);
+}
+export const callPublicFloat = (publicName: string, paramTypes: string, ...args: Array<any>): any => {
+    return samp.callPublicFloat(publicName, paramTypes, ...args);
+}
+export const logprint = (str: string): any => {
+    return samp.logprint(str);
+}
+export const callNative = (nativeName: string, paramTypes: string, ...args: Array<any>): any => {
+    return samp.callNative(nativeName, paramTypes, ...args);
+}
+export const callNativeFloat = (nativeName: string, paramTypes: string, ...args: Array<any>): any => {
+    return samp.callNativeFloat(nativeName, paramTypes, ...args);
 }
