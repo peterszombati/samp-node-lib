a82c6ea568272b54fc8dce60b33ac2c61ede332a
Peter Szombati <szombatipeti7@gmail.com>
2022-04-23 15:07:31 +0200

    .

---
+++ @types/sa-mp/globals.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for samp-node
+// Project: @sa-mp/node
+// Definitions by: pkfln <https://github.com/pkfln>
+
+declare const __resname: string;
---
+++ @types/sa-mp/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="samp.d.ts" />
+/// <reference path="events.d.ts" />
+/// <reference path="globals.d.ts" />
