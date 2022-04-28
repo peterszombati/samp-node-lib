8dd450c6bfb793ff991e76ed20332ac674eb77c3
Peter Szombati <szombatipeti7@gmail.com>
2022-04-28 09:09:55 +0200

    accept hex color

--- src/rgba.ts
+++ src/rgba.ts
@@ -1,6 +1,25 @@
 export function rgba(value: string): number {
+    if (value.charAt(0) === '#') {
+        if (value.length === 4) {
+            return parseInt(
+                value.charAt(1)
+                +  value.charAt(1)
+                +  value.charAt(2)
+                +  value.charAt(2)
+                +  value.charAt(3)
+                +  value.charAt(3)
+                + 'FF',16)
+        } else if (value.length === 7) {
+            return parseInt(value.slice(1, value.length) + 'FF',16)
+        } else if (value.length === 9) {
+            return parseInt(value.slice(1, value.length),16)
+        }
+        //TODO console.warn(new Error("invalid hex color:" + JSON.stringify({value,time:new Date().toISOString()})))
+        return 255
+    }
     const values: number[] = (value.match(/([0-9])+/g) || []).map(v => parseInt(v, 10))
     if (values.length !== 4 && values.length !== 3) {
+        //TODO console.warn(new Error("invalid rgb color:" + JSON.stringify({value,time:new Date().toISOString()})))
         return 255
     }
     if (values.length === 3) {
