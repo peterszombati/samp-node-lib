6f1212efa8c42db5afb25bd802a19352b1693eef
Peter Szombati <szombatipeti7@gmail.com>
2022-04-28 08:47:04 +0200

    refactor

--- src/rgba.ts
+++ src/rgba.ts
@@ -1,18 +1,18 @@
 export function rgba(value: string): number {
-    const values: number[] = value.split('(')[1].split(')')[0].split(',').map(v => parseInt(v, 10));
+    const values: number[] = (value.match(/([0-9])+/g) || []).map(v => parseInt(v, 10))
     if (values.length !== 4 && values.length !== 3) {
-        return 255;
+        return 255
     }
     if (values.length === 3) {
-        values.push(255);
+        values.push(255)
     } else {
-        values[3] = values[3] < 1 ? Math.floor(values[3] * 255) : 255;
+        values[3] = values[3] < 1 ? Math.floor(values[3] * 255) : 255
     }
-    let n = 0;
+    let n = 0
     values.reverse().forEach((n2, i) => {
         if (n2 !== 0) {
             n += Math.pow(16, i * 2) * n2
         }
-    });
-    return n;
+    })
+    return n
 }
