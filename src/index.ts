d40f535ea1610ccfbf1230ca1cee2c0f247dc584
Peter Szombati <szombatipeti7@gmail.com>
2022-06-23 22:25:58 +0200

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ export {getPlayers} from "./getPlayers";
 export {rgba} from "./rgba";
 export * as ComponentCosts from './ComponentCosts';
 export * as ValidComponents from './ValidComponents';
+export * from "./SampVehicles";
+export * from "./SampPlayers";
+export * from "./SampVehicle";
 
 // legacy exports
 export * as SampFunctions from './SampFunctions';
